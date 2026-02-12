0c36ef3037ddbde98f1e9d00af2caf22d4a06f2f
T117m <timofey.goryachev@proton.me>
2026-02-13 02:20:34 +0300

    add Product()

--- matrix.go
+++ matrix.go
@@ -27,6 +27,21 @@ func CompareSize(a, b Matrix) bool {
 }
 
 func Product(a, b Matrix) Matrix {
+	if !CompareSize(a, b) {
+		return NewMatrix(0)
+	}
+
+	c := NewMatrix(len(a))
+
+	for i := range a {
+		for j := range b {
+			for k := range c {
+				c[i][j] += a[i][k] * b[k][j]
+			}
+		}
+	}
+
+	return c
 }
 
 func Inverse(a Matrix) Matrix {
