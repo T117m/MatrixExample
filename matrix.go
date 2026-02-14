0db012a51b65483841d9b041e85b62c6b97cc215
T117m <timofey.goryachev@proton.me>
2026-02-14 17:02:04 +0300

    change int to float64 for accurate division and muliplication calculations

--- matrix.go
+++ matrix.go
@@ -2,13 +2,13 @@ package main
 
 import "math"
 
-type Matrix [][]int
+type Matrix [][]float64
 
 func NewMatrix(n int) Matrix {
 	a := make(Matrix, n)
 
 	for i := range a {
-		a[i] = make([]int, n)
+		a[i] = make([]float64, n)
 	}
 
 	return a
@@ -46,7 +46,7 @@ func Product(a, b Matrix) Matrix {
 	return c
 }
 
-func simpleDet(a Matrix) int {
+func simpleDet(a Matrix) float64 {
 	if len(a) == 1 {
 		return a[0][0]
 	}
@@ -54,7 +54,7 @@ func simpleDet(a Matrix) int {
 	return a[0][0]*a[1][1] - a[0][1]*a[1][0]
 }
 
-func minor(a Matrix, x, y int) int {
+func minor(a Matrix, x, y int) float64 {
 	n := len(a)
 
 	if n < 3 {
@@ -73,18 +73,18 @@ func minor(a Matrix, x, y int) int {
 	return determinant(m)
 }
 
-func cofactor(a Matrix, x, y int) int {
-	return int(math.Pow(float64(-1), float64(x+y))) * a[x][y] * minor(a, x, y)
+func cofactor(a Matrix, x, y int) float64 {
+	return math.Pow(float64(-1), float64(x+y)) * a[x][y] * minor(a, x, y)
 }
 
-func determinant(a Matrix) int {
+func determinant(a Matrix) float64 {
 	n := len(a)
 
 	if n < 3 {
 		return simpleDet(a)
 	}
 
-	var det int
+	var det float64
 
 	for j := range n {
 		det += cofactor(a, 0, j)
